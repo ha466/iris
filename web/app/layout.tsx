6ae5962d11f2d166c2fcf06fce31b18efebd5c2b
Evking <58391348+ha466@users.noreply.github.com>
2024-12-23 19:18:06 +0530

    Update layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { usePathname } from 'next/navigation'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,10 +14,18 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const pathname = usePathname()
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <link
+          rel="stylesheet"
+          href={`${pathname === '/' ? '' : pathname}/globals.css`}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
 }
+
 
