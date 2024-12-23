e2a931f2c3960464c154fa78f85ea78c4cb1a2df
Evking <58391348+ha466@users.noreply.github.com>
2024-12-23 20:10:59 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: '/iris/',
+  basePath: '/iris',
+}
+
+module.exports = nextConfig
+
